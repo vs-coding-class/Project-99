8fbb4ce19cf8ab00990f0864bf42c080b222eff9
vs-coding-class <65456951+vs-coding-class@users.noreply.github.com>
2021-07-24 10:29:31 -0400

    Update Project_99.py

--- Project_99.py
+++ Project_99.py
@@ -1,12 +1,13 @@
 import os
 import shutil
+import time
 
 path = input('Please enter the path of the folder that you would like to clean up... ')
 days = input('Please enter the number of days you would like to organize your folder by... ')
 
-nanoseconds = int(days) * 24 * 3600000000000
-print(nanoseconds)
-os.mkdir('C:/Users/vinay/Desktop/Older_than_'+days+'_days')
+seconds = int(days) * 24 * 3600
+print(seconds)
+os.mkdir(path+'/'+days+'_days')
 
 for (root, dirs, files) in os.walk(path, topdown = True):
     for name in files:
@@ -14,13 +15,13 @@ for (root, dirs, files) in os.walk(path, topdown = True):
         print(file_name)
         ctime_file = os.stat(file_name).st_ctime
         print(ctime_file)
-        if(ctime_file > nanoseconds):
-            os.remove(file_name)
+        if(ctime_file < time.time() - seconds):
+            destination2 = shutil.move(file_name, path+'/'+days+'_days')
 
     for name in dirs:
         dir_name = os.path.join(root, name)
         print(dir_name)
         ctime_dir = os.stat(dir_name).st_ctime
         print(ctime_dir)
-        if(ctime_dir > nanoseconds):
-            shutil.rmtree(dir_name)
+        if(ctime_dir < time.time() - seconds):
+            destination = shutil.copytree(dir_name, path+'/'+days+'_days')
